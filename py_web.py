02f2a4f3326819cd5743a50bf0987e81273c6500
TheThreeBody <29009714+TheThreeBody@users.noreply.github.com>
2021-10-20 19:38:19 +0800

    first py web

---
+++ py_web.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from flask import request
+from real_url.douyu import DouYu
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/douyu', methods=['get'])
+def response_douyu():
+
+    args = request.args.get("roomnum")
+    form = request.form.get('data')
+    roomnum = args
+    douyu = DouYu(roomnum)
+    data = {
+        "stream" : douyu.get_real_url()
+    }
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run()
